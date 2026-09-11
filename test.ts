c86aef61a7a4988721f682898db28323bf924000
RavinakS <ravinasingh697@gmail.com>
2026-09-11 17:17:18 +0530

    find triplets from nums array

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
-console.log("Happy coding!");
+function* idGenerator() {
+  let id = 1;
+  while (true) {
+    yield `id_${id}`; // The function freezes right here!
+    id++;
+  }
+}
+
+const idStream = idGenerator();
+
+console.log(idStream.next().value); // "id_1"
+console.log(idStream.next().value); // "id_2"
+console.log(idStream.next().value); // "id_3"
